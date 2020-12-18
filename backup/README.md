37dc4c0cf9cc58a02dfcf9abc0b3c77369da24be
Kateleigh <klorimer21@gmail.com>
2020-12-17 20:28:48 -0500

    Darrin Move

---
+++ backup/README.md
@@ -0,0 +1,1 @@
+# the-blog-clog-frontend
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
